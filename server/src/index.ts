d59c0c35dad5838f41d5fa348eed4887393d7882
dogukanVaris <varisdogukan@outlook.com>
2024-05-10 10:13:43 +0300

    Testig vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,11 +15,13 @@ const dataFilePath = path.join(__dirname, "data.json");
 const rawData = readFileSync(dataFilePath);
 export const jobsData: JobType[] = JSON.parse(rawData.toString());
 
-app.get("/", (req: Request, res: Response) => {
+const base = "/api/v1";
+
+app.get(`${base}`, (req: Request, res: Response) => {
   res.send("Welcome to devJobs api");
 });
-app.get("/devjobs", getJobs);
-app.get("/devjobs/:id", getSingleJob);
+app.get(`${base}/devjobs`, getJobs);
+app.get(`${base}/devjobs/:id`, getSingleJob);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
