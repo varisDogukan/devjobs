a4fbb38f1eae67efd8f94609553e7cac68fae31b
dogukanVaris <varisdogukan@outlook.com>
2024-05-10 10:25:15 +0300

    Testing vercel 5

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,13 +19,11 @@ const dataFilePath = path.join(__dirname, "data.json");
 const rawData = readFileSync(dataFilePath);
 export const jobsData: JobType[] = JSON.parse(rawData.toString());
 
-const base = "/api/v1";
-
-app.get(`${base}`, (req: Request, res: Response) => {
-  res.send("Welcome to devJobs api");
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Welcome to devjobs api</h1>");
 });
-app.get(`${base}/devjobs`, getJobs);
-app.get(`${base}/devjobs/:id`, getSingleJob);
+app.get("/devjobs", getJobs);
+app.get("/devjobs/:id", getSingleJob);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
